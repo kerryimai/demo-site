1ffe35c0c760764303568ee58753c8ac10288867
Robert Balicki <robert.balicki@gmail.com>
2019-09-25 21:57:22 -0400

    ticketmaster yo

--- public/ticketmaster.js
+++ public/ticketmaster.js
@@ -2,7 +2,6 @@ document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
   const numberOfTickets = document.getElementById('numberOfTickets').value;
   const withDiscount = document.getElementById('discount').checked;
-  console.log(withDiscount);
   document.getElementById('results').innerHTML = `
     You purchased ${numberOfTickets} tickets with ${withDiscount ? 'a' : 'no'} discount.
     Thank you! Enjoy the Jonas Brothers concert!
